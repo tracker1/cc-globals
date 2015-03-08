1ce449a0e396b58b3299287de6ee0350085d4555
Michael J. Ryan <tracker1@gmail.com>
2015-03-08 14:49:00 -0700

    force init

--- index.js
+++ index.js
@@ -2,14 +2,4 @@ var R = require('ramda');
 var fetch = require('isomorphic-fetch');
 var env = this;
 
-module.exports = {
-  ramda: R
-  ,fetch: fetch
-  ,load: loadGlobals
-};
-
-function loadGlobals() {
-    module.exports.load = function(){}; //only load once
-    R.installTo(env);
-    env.fetch = fetch;
-};
+R.installTo(typeof(global) !== 'undefined' ? global : this);
