0b6a49df12d1d2232954b9a3f4157d21a3abfd23
Michael J. Ryan <tracker1@gmail.com>
2015-04-21 21:08:16 +0000

    Add promise implementation, force to bluebird.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cc-globals
-Setup global variables for next generation projects (ramda and fetch)
+
+Setup global variables for next generation projects.
+
+* `R` - Ramda
+* `Promise` - bluebird (also sets `i-promise`)
+* `fetch` - isomorphic-fetch
--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var R = require('ramda');
-var fetch = require('isomorphic-fetch');
+var R = require('ramda'); 
+var fetch = require('isomorphic-fetch'); //self patching global
+var bb = require('bluebird');
 
+//patch global Ramda
 if (typeof global !== 'undefined') global.R = R;
 this.R = R;
+
+//make i-promise use bluebird
+require('i-promise/config').use(bb);
+
+//make global Promise implementation bluebird
+this.Promise = bb;
+
+//export the modules this uses
+module.exports = {
+  R: R,
+  fetch: fetch,
+  Promise: bb
+};
